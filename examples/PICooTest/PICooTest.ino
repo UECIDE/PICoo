6ead8a0d79dfa4054c8ed7d5850bae8b3f40529b
Matt Jenkins <matt@majenko.co.uk>
2014-01-26 22:33:16 +0000

    Added example sketch

---
+++ examples/PICooTest/PICooTest.ino
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+IO::PIC32 Core;
+IO::Pin LED(Core, 13, IO::OUTPUT, IO::LOW);
+
+const char states[] = {'Z', 'R', 'S', 'H', 'M'};
+
+thread blnk;
+thread topscreen;
+
+void setup() {
+	blnk = Thread::Create("blink",blinker, 0, 512);
+	topscreen = Thread::Create("top",top, 0, 512);
+	printf("\e[2J");
+}
+
+uint32_t spoon;
+
+void blinker(uint32_t x) {
+	for (i = 0; i < 10; i++) {
+		LED.write(IO::HIGH);
+		Thread::Sleep(100);
+		LED.write(IO::LOW);
+		Thread::Sleep(500);
+	}
+}
+
+extern thread ThreadList;
+extern thread currentThread;
+
+void top(uint32_t x) {
+	while(1) {
+	    printf("\e[0;0H");
+	    printf("Uptime: %lums\n", Thread::Milliseconds());
+	    printf("Entry    Thread     Stack %%CPU S A\n");
+	    for (struct TCB *scan = ThreadList; scan; scan = scan->next) {
+	        printf("%08X %-10s %-4d %4d%% %c %c\n",
+	            scan->entry,
+	            scan->name, (scan->stack_head - scan->sp) * 4, 
+	            Thread::Runtime(scan) * 100 / Thread::Milliseconds(),
+	            states[scan->state],
+	            scan == currentThread? '*' : ' '
+	        );
+	    }
+		Thread::Sleep(100);
+	}
+}
